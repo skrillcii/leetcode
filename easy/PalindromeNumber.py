79eac7b91fbb6237ce1590cf6b1defed0f9ac125
AA373084 <aa373084@AA373084s-MacBook-Pro.local>
2020-01-14 19:39:37 +0900

    update

--- easy/PalindromeNumber.py
+++ easy/PalindromeNumber.py
@@ -1,28 +1,23 @@
+#!/usr/local/bin/python3
 
 class Solution:
 
     def isPalindrome(self, x:int) -> bool:
-
         if x == 0: return True
-
         elif x > 0 and x%10 != 0:
             r = 0
-            o = x
-            while o > r:
-                print(r)
+            while x > r:
                 d = x%10
-                r = r*ldkfj0
+                r = r*10+d
                 x = (x-d)/10
-                print('x: {}, d: {}, r:{}'.format(x, r, d))
-
-            #if x == r or x == r/10:
-            return True
-
+                print('x: {}, r:{}'.format(x, r))
+            if x == r or x == (r-r%10)/10: return True
+            else: return False
         else: return False
 
 
 if __name__ == '__main__':
-    x = 121
+    x = 31213
     solution = Solution()
     answer = solution.isPalindrome(x)
     print(answer)
