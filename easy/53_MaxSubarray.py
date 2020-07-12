dcc702ae30c6bafb396eb9b9ac64a84ee17f0b83
morikuni <shu.morikuni@ibm.com>
2020-07-13 01:21:06 +0900

    update

---
+++ easy/53_MaxSubarray.py
@@ -0,0 +1,28 @@
+
+class Solution:
+    def maxSubArray(self, nums: list) -> int:
+
+        import ipdb
+        ipdb.set_trace()
+
+        output = []
+        if len(nums) == 1:
+            output = nums
+
+        sum_ = []
+        for idx in range(len(nums)):
+            if idx == 0:
+                sum_.append(nums[idx])
+            else:
+                cur_value = nums[idx]
+                sum_with_prev = cur_value + sum_[idx - 1]
+                sum_.append(max([cur_value, sum_with_prev]))
+        output = max(sum_)
+        return output
+
+
+if __name__ == '__main__':
+    x = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    solution = Solution()
+    answer = solution.maxSubArray(x)
+    print(answer)
