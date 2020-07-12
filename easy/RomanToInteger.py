2a70dc9769f7a1f194f19ed90eed72d3ff1603bb
morikuni <shu.morikuni@ibm.com>
2020-07-12 21:15:46 +0900

    update

--- easy/RomanToInteger.py
+++ easy/RomanToInteger.py
@@ -1,25 +1,38 @@
 
 class Solution:
 
-    Pairs = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000,
-            }
+    def __init__(self):
+        self.Rom2Int = {'I': 1,
+                        'V': 5,
+                        'X': 10,
+                        'L': 50,
+                        'C': 100,
+                        'D': 500,
+                        'M': 1000
+                        }
 
-    def romanToInteger(self, s:str) -> int:
+    def romanToInteger(self, s: str) -> int:
 
-        import ipdb
-        ipdb.set_trace()
+        int_ = 0
+        if set(s) == 'I':
+            for idx, r in enumerate(s):
+                int_ += self.Rom2Int[r]
 
-        return False
+        else:
+            for idx, r in enumerate(s):
+                if len(s[idx:]) >= 2:
+                    if self.Rom2Int[r] >= self.Rom2Int[s[idx + 1]]:
+                        int_ += self.Rom2Int[r]
+                    else:
+                        int_ -= self.Rom2Int[r]
+                else:
+                    int_ += self.Rom2Int[r]
+
+        return int_
+
 
 if __name__ == '__main__':
-    s = 'XI'
+    s = 'MCMXCIV'
     solution = Solution()
     answer = solution.romanToInteger(s)
     print(answer)
