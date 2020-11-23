043a8a8bb5573f694f6c54fa6f1461c00e04b2dc
skrillcii <skrillcii.github.com>
2020-11-23 22:13:10 +0900

    updated

--- easy/1413_MinStartValue.py
+++ easy/1413_MinStartValue.py
@@ -5,9 +5,21 @@ import heapq
 class Solution:
 
     def minStartValue(self, nums: list) -> int:
+        i = 1
         while True:
+            sum_ = self.loopSum(i, nums)
+            if not sum_:
+                i += 1
+            else:
+                return i
 
-        return None
+    def loopSum(self, sum_, nums):
+        for j in range(len(nums)):
+            sum_ = sum_ + nums[j]
+            if sum_ < 1:
+                return False
+        else:
+            return sum_
 
 
 if __name__ == '__main__':
