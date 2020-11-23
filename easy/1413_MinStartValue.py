80e776ba415822c886352b3f6cd1b4cc402bc05d
morikuni <shu.morikuni@ibm.com>
2020-11-23 21:28:53 +0900

    updated

---
+++ easy/1413_MinStartValue.py
@@ -0,0 +1,22 @@
+import collections
+import heapq
+
+
+class Solution:
+
+    def minStartValue(self, nums: list) -> int:
+        while True:
+
+        return None
+
+
+if __name__ == '__main__':
+
+    import ipdb
+    ipdb.set_trace()
+
+    nums = [-3, 2, -3, 4, 2]
+
+    solution = Solution()
+    answer = solution.minStartValue(nums)
+    print(answer)
