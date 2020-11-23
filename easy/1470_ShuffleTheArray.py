610f49d27fd485306142c58760e9f96989b2f5cd
morikuni <shu.morikuni@ibm.com>
2020-11-23 16:14:45 +0900

    updated

---
+++ easy/1470_ShuffleTheArray.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def shuffleTheArray(self, nums: list, n: int) -> list:
+        '''
+        Time Complexity = (N)
+        Space Complexity = (N)
+        '''
+        list_ = []
+        for head1 in range(n):
+            list_.append(nums[head1])
+            list_.append(nums[head1+n])
+        return list_
+
+
+if __name__ == '__main__':
+
+    # import ipdb
+    # ipdb.set_trace()
+
+    nums = [2, 5, 1, 3, 4, 7]
+    nums = [1, 2, 3, 4, 4, 3, 2, 1]
+    nums = [1, 1, 2, 2]
+
+    solution = Solution()
+    answer = solution.shuffleTheArray(nums, int(len(nums)/2))
+    print(answer)
