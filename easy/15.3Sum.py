bd90c5d8ac24d7e2c7aafe828051a4553f7d0038
morikuni <shu.morikuni@ibm.com>
2020-11-23 14:10:07 +0900

    fix

--- easy/15.3Sum.py
+++ easy/15.3Sum.py
@@ -2,7 +2,7 @@ class Solution:
 
     def threeSum(self, nums: list) -> list:
 
-        if nums == [] or nums == [0] or len(nums) < 3:
+        if nums == [] or nums == [0] or len(nums) <= 2:
             return []
 
         else:
@@ -11,7 +11,7 @@ class Solution:
             for idx, target in enumerate(nums):
                 if target > 0:
                     break
-                elif nums[idx] != nums[idx - 1]:
+                elif idx == 0 or nums[idx] != nums[idx - 1]:
                     self.twoSum(nums, idx, target, solutions)
 
         return solutions
@@ -21,7 +21,7 @@ class Solution:
         p1 = idx + 1
         p2 = len(nums) - 1
 
-        while nums[p1] < nums[p2]:
+        while p1 < p2:
             if nums[p1] + nums[p2] + target == 0:
                 solutions.append([target, nums[p1], nums[p2]])
                 p1 += 1
