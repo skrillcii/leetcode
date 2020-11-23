76e9d89a5f0f631b16f53bd2e27cbfa402477ee7
morikuni <shu.morikuni@ibm.com>
2020-11-23 13:18:23 +0900

    updated 3sum

--- easy/15.3Sum.py
+++ easy/15.3Sum.py
@@ -6,6 +6,32 @@ class Solution:
             return []
 
         nums = sorted(nums)
+        solutions = []
+
+        for idx, target in enumerate(nums):
+
+            if target > 0:
+                break
+
+            p1 = idx + 1
+            p2 = -1
+
+            while  nums[p1] < nums[p2]:
+                if target == nums[idx - 1]:
+                    break
+
+                elif nums[p1] + nums[p2] + target == 0:
+                    solutions.append([target, nums[p1], nums[p2]])
+                    p1 += 1
+                    p2 -= 1
+
+                elif nums[p1] + nums[p2] < target:
+                    p1 += 1
+
+                elif nums[p1] + nums[p2] > target:
+                    p2 -= 1
+
+        return solutions
 
 
 if __name__ == '__main__':
