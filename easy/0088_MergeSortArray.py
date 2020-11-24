6c8ba0c569e1dbc487b4ae9f1b0ba4797bb87682
skrillcii <skrillcii.github.com>
2020-11-24 09:18:06 +0900

    udpated

--- easy/0088_MergeSortArray.py
+++ easy/0088_MergeSortArray.py
@@ -5,24 +5,19 @@ class Solution:
         Time Complexity: O(N+M)
         Space Complexity: O(1)
         '''
-        p = -1
+        p = m + n - 1
         p1 = m - 1
-        p2 = -1
+        p2 = n - 1
 
-        while p2 >= -n:
+        while p1 >= 0 and p2 >= 0:
             if nums1[p1] < nums2[p2]:
                 nums1[p] = nums2[p2]
-                p -= 1
                 p2 -= 1
-            elif nums1[p1] > nums2[p2]:
-                temp = nums1[p]
+            else:
                 nums1[p] = nums1[p1]
-                nums1[p1] = temp
-                p -= 1
-            elif nums1[p1] == nums2[p2]:
-                nums1[p] = nums2[p2]
-                p -= 1
-                p2 -= 1
+                p1 -= 1
+            p -= 1
+        nums1[:p2 + 1] = nums2[:p2 + 1]
         return nums1
 
 
@@ -53,4 +48,16 @@ if __name__ == '__main__':
     nums2 = [1]
     solution = Solution()
     answer = solution.merge(nums1, 1, nums2, len(nums2))
+    print(answer)
+
+    nums1 = [4, 5, 6, 0, 0, 0]
+    nums2 = [1, 2, 3]
+    solution = Solution()
+    answer = solution.merge(nums1, 3, nums2, len(nums2))
+    print(answer)
+
+    nums1 = [0, 0, 0, 0, 0]
+    nums2 = [1, 2, 3, 4, 5]
+    solution = Solution()
+    answer = solution.merge(nums1, 0, nums2, len(nums2))
     print(answer)
