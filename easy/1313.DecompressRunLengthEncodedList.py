192b6a90ee7738ece229fdb06e25b97ed2bf53fb
skrillcii <skrillcii.github.com>
2020-11-27 15:55:22 +0900

    updated

---
+++ easy/1313.DecompressRunLengthEncodedList.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def decompressRLElist(self, nums: list) -> list:
+        '''
+        Time Complexity = (N)
+        Space Complexity = (N)
+        '''
+        if nums == []:
+            return []
+
+        list_ = []
+        for i in range(len(nums) // 2):
+            freq = nums[2 * i]
+            val = nums[2 * i + 1]
+            sub_list = [val] * freq
+            list_.extend(sub_list)
+        return list_
+
+
+if __name__ == '__main__':
+
+    import ipdb
+    ipdb.set_trace()
+
+    nums = [1, 2, 3, 4]
+
+    solution = Solution()
+    answer = solution.decompressRLElist(nums)
+    print(answer)
