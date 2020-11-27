151079f66c8b23ea0e8edbcb2175ae4e2a499b78
skrillcii <skrillcii.github.com>
2020-11-27 17:00:23 +0900

    updated

--- easy/0977_SquaresOfASortedArrays.py
+++ easy/0977_SquaresOfASortedArrays.py
@@ -1,8 +1,8 @@
 class Solution:
 
-    def sortedSquares(self, A: list) -> list:
+    def sortedSquares_1st_trial(self, A: list) -> list:
         '''
-        Time Complexity = (N)
+        Time Complexity = (NlogN)
         Space Complexity = (N)
         '''
         list_ = []
@@ -16,6 +16,46 @@ class Solution:
         return list_
 
 
+    def sortedSquares_two_pointers(self, A: list) -> list:
+        '''
+        Time Complexity = (N)
+        Space Complexity = (N)
+        '''
+        list_ = []
+
+        if A == []:
+            return list_
+
+        N = len(A) - 1
+        i = 0
+        j = 0
+
+        for _i, a in enumerate(A):
+            if a >= 0:
+                j = _i - 1
+                i = _i
+                break
+
+        while j >= 0 and i <= N:
+            if A[j] ** 2 < A[i] ** 2:
+                list_.append(A[j] ** 2)
+                j -= 1
+            elif A[j] ** 2 > A[i] ** 2:
+                list_.append(A[i] ** 2)
+                i += 1
+
+        while j >= 0:
+            list_.append(A[j] ** 2)
+            j -= 1
+
+        while i <= N:
+            list_.append(A[i] ** 2)
+            i += 1
+
+        return list_
+
+
+
 if __name__ == '__main__':
 
     import ipdb
@@ -23,5 +63,5 @@ if __name__ == '__main__':
 
     A = [-4, -1, 0, 3, 10]
     solution = Solution()
-    answer = solution.sortedSquares(A)
+    answer = solution.sortedSquares_two_pointers(A)
     print(answer)
