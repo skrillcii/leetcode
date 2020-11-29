4c82f214641ffbbf7bc56f77b7454a70e77a672b
morikuni <shu.morikuni@ibm.com>
2020-11-29 13:23:21 +0900

    updated

---
+++ easy/1002_FindCommonCharacters.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def commonChars(self, A: list) -> list:
+        '''
+        Time Complexity = O(N)
+        Space Complexity = O(N)
+        '''
+        ref = min(A, key=len)
+        list_ = []
+        for char in ref:
+            bool_ = all([char in string for string in A])
+            if bool_:
+                list_.append(char)
+        return list_
+
+
+if __name__ == '__main__':
+
+    import ipdb
+    ipdb.set_trace()
+
+    A = ["bella", "label", "roller"]
+    solution = Solution()
+    answer = solution.commonChars(A)
+    print(solution)
+
+    A = ["cool", "lock", "cook"]
+    solution = Solution()
+    answer = solution.commonChars(A)
+    print(solution)
