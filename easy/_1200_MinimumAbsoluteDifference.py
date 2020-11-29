069e8723eb8c1cfbeb5881a17f117ce5e514f2c3
morikuni <shu.morikuni@ibm.com>
2020-11-29 22:27:38 +0900

    updated

---
+++ easy/_1200_MinimumAbsoluteDifference.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def minimumAnsDifferenve(self, arr: list) -> int:
+        '''
+        Time Complexity = O(N)
+        Space Complexity = O(N)
+        '''
+        return 1
+
+
+if __name__ == '__main__':
+
+    import ipdb
+    ipdb.set_trace()
+
+    arr = [4, 2, 1, 3]
+    solution = Solution()
+    answer = solution.minimumAnsDifferenve(arr)
+    print(answer)
