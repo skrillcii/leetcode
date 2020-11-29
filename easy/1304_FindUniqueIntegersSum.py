70092ae65f20d888b4d15ce898215a12ab5c9458
morikuni <shu.morikuni@ibm.com>
2020-11-29 22:15:59 +0900

    updated

--- easy/1304_FindUniqueIntegersSum.py
+++ easy/1304_FindUniqueIntegersSum.py
@@ -1,13 +1,13 @@
 class Solution:
 
-    def sumZero(self, n: int) -> list:
+    def sumZero_1st_trial(self, n: int) -> list:
         '''
          Time Complexity = O(N)
         Space Complexity = O(N)
         '''
         list_ = []
         if n % 2 == 0:
-            for i in range(n / 2):
+            for i in range(n // 2):
                 list_.append(i + 1)
                 list_.append(-i - 1)
         else:
@@ -17,6 +17,13 @@ class Solution:
             list_.append(0)
         return list_
 
+    def sumZero_wave_out(self, n: int) -> list:
+        '''
+        Time Complexity = O(N)
+        Space Complexity = O(N)
+        '''
+        return [i for i in range(1 - n, n, 2)]
+
 
 if __name__ == '__main__':
 
@@ -25,15 +32,30 @@ if __name__ == '__main__':
 
     n = 5
     solution = Solution()
-    answer = solution.sumZero(n)
+    answer = solution.sumZero_1st_trial(n)
     print(answer)
 
     n = 3
     solution = Solution()
-    answer = solution.sumZero(n)
+    answer = solution.sumZero_1st_trial(n)
     print(answer)
 
     n = 1
     solution = Solution()
-    answer = solution.sumZero(n)
+    answer = solution.sumZero_1st_trial(n)
+    print(answer)
+
+    n = 5
+    solution = Solution()
+    answer = solution.sumZero_wave_out(n)
+    print(answer)
+
+    n = 3
+    solution = Solution()
+    answer = solution.sumZero_wave_out(n)
+    print(answer)
+
+    n = 1
+    solution = Solution()
+    answer = solution.sumZero_wave_out(n)
     print(answer)
