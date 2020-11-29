c69f01a5f5aeae47a0638690119435ecf56021a8
morikuni <shu.morikuni@ibm.com>
2020-11-29 14:35:46 +0900

    updated

--- easy/1512_NumberOfGoodPairs.py
+++ easy/1512_NumberOfGoodPairs.py
@@ -1,9 +1,9 @@
 class Solution:
 
-    def numIdenticalPairs(self, nums: list) -> int:
+    def numIdenticalPairs_double_loop(self, nums: list) -> int:
         '''
-        Time Complexity = O(N)
-        Space Complexity = O(N)
+        Time Complexity = O(Nx(N-1))
+        Space Complexity = O(1)
         '''
         counter = 0
         for i in range(len(nums) - 1):
@@ -11,6 +11,25 @@ class Solution:
                 if nums[i] == nums[j]:
                     counter += 1
         return counter
+
+    def numIdenticalPairs_hash_table(self, nums: list) -> int:
+        '''
+        Time Complexity = O(Nx(N))
+        Space Complexity = O(1)
+        '''
+        dict_ = dict()
+        for num in nums:
+            if num not in dict_.keys():
+                dict_[num] = 0
+            else:
+                dict_[num] += 1
+        return sum([self.__combinatiom(count) for count in dict_.values()])
+
+    def __combinatiom(self, count):
+        combinations = 0
+        for i in range(count + 1):
+            combinations += i
+        return combinations
 
 
 if __name__ == '__main__':
@@ -20,15 +39,30 @@ if __name__ == '__main__':
 
     nums = [1, 2, 3, 1, 1, 3]
     solution = Solution()
-    answer = solution.numIdenticalPairs(nums)
+    answer = solution.numIdenticalPairs_double_loop(nums)
     print(answer)
 
     nums = [1, 1, 1, 1]
     solution = Solution()
-    answer = solution.numIdenticalPairs(nums)
+    answer = solution.numIdenticalPairs_double_loop(nums)
     print(answer)
 
     nums = [1, 2, 3]
     solution = Solution()
-    answer = solution.numIdenticalPairs(nums)
+    answer = solution.numIdenticalPairs_double_loop(nums)
+    print(answer)
+
+    nums = [1, 2, 3, 1, 1, 3]
+    solution = Solution()
+    answer = solution.numIdenticalPairs_hash_table(nums)
+    print(answer)
+
+    nums = [1, 1, 1, 1]
+    solution = Solution()
+    answer = solution.numIdenticalPairs_hash_table(nums)
+    print(answer)
+
+    nums = [1, 2, 3]
+    solution = Solution()
+    answer = solution.numIdenticalPairs_hash_table(nums)
     print(answer)
