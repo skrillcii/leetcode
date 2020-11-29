175cdd548a4b424659da46c6f5c32b031fb1695c
morikuni <shu.morikuni@ibm.com>
2020-11-29 10:55:37 +0900

    updated

---
+++ easy/0066_PlusOne.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def plusOne(self, digits: list) -> list:
+        '''
+        Time Complexity = O(N)
+        Space Complexity = O(N)
+        '''
+        for i in range(-1, -len(digits) - 1, -1):
+            if digits[i] == 9:
+                digits[i] == 0
+            else:
+                digits[i] += 1
+                return digits
+
+
+if __name__ == '__main__':
+
+    import ipdb
+    ipdb.set_trace()
+
+    digits = [1, 2, 3]
+    solution = Solution()
+    answer = solution.plusOne(digits)
+    print(answer)
