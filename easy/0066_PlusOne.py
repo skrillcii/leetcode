c0d2d08f970a8fb4729df1342cbdf9aac2111b39
morikuni <shu.morikuni@ibm.com>
2020-11-29 11:19:47 +0900

    updated

--- easy/0066_PlusOne.py
+++ easy/0066_PlusOne.py
@@ -7,7 +7,10 @@ class Solution:
         '''
         for i in range(-1, -len(digits) - 1, -1):
             if digits[i] == 9:
-                digits[i] == 0
+                digits[i] = 0
+                if i == -len(digits):
+                    digits.insert(0, 1)
+                    return digits
             else:
                 digits[i] += 1
                 return digits
@@ -19,6 +22,11 @@ if __name__ == '__main__':
     ipdb.set_trace()
 
     digits = [1, 2, 3]
+    solution = Solution()
+    answer = solution.plusOne(digits)
+    print(answer)
+
+    digits = [9]
     solution = Solution()
     answer = solution.plusOne(digits)
     print(answer)
