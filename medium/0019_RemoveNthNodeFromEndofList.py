6fd4e34205f1ce517d2ff74311dcedfb4841cb8a
skrillcii <skrillcii.github.com>
2021-01-27 21:40:07 +0900

    updated

--- medium/0019_RemoveNthNodeFromEndofList.py
+++ medium/0019_RemoveNthNodeFromEndofList.py
@@ -19,14 +19,23 @@ class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         '''
         Time Complexity = O(N)
-        Space Complexity = O(N)
+        Space Complexity = O(1)
         '''
-        return None
+        slow = fast = head
+        for _ in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return head
 
     def removeNthFromEnd_value_shifting(self, head: ListNode, n: int) -> ListNode:
         '''
         Time Complexity = O(N)
-        Space Complexity = O(N)
+        Space Complexity = O(1)
         '''
 
         '''
@@ -39,9 +48,58 @@ class Solution:
         '''
 
         def index(node):
+            if not node:
+                return 0
+            i = index(node.next) + 1
+            if i > n:
+                node.next.val = node.val
             return i
         index(head)
         return head.next
+
+    def removeNthFromEnd_index_and_remove(self, head: ListNode, n: int) -> ListNode:
+        '''
+        Time Complexity = O(N)
+        Space Complexity = O(1)
+        '''
+
+        '''
+        LeetCode Post:
+            'In this solution I recursively determine the indexes again,
+            but this time my helper function removes the nth node.  It returns two values.
+            The index, as in my first solution,
+            and the possibly changed head of the remaining list.'
+        '''
+
+        def remove(head):
+            if not head:
+                return 0, head
+            i, head.next = remove(head.next)
+            return i + 1, (head, head.next)[i + 1 == n]
+        return remove(head)[1]
+
+    def removeNthFromEnd_n_ahead(self, head: ListNode, n: int) -> ListNode:
+        '''
+        Time Complexity = O(N)
+        Space Complexity = O(1)
+        '''
+
+        '''
+        LeetCode Post:
+            'The standard solution, but without a dummy extra node. 
+            Instead, I simply handle the special case of removing the head 
+            right after the fast cursor got its head start.'
+        '''
+        fast = slow = head
+        for _ in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
 
 
 if __name__ == '__main__':
@@ -53,19 +111,19 @@ if __name__ == '__main__':
     list_ = [1, 2, 3, 4, 5]
     head = LinkedList(list_).head
     solution = Solution()
-    answer = solution.removeNthFromEnd_recursive_shift(head, n)
+    answer = solution.removeNthFromEnd_value_shifting(head, n)
     print(answer)
 
     n = 1
     list_ = [1]
     head = LinkedList(list_).head
     solution = Solution()
-    answer = solution.removeNthFromEnd_recursive_shift(head, n)
+    answer = solution.removeNthFromEnd_value_shifting(head, n)
     print(answer)
 
     n = 1
     list_ = [1, 2]
     head = LinkedList(list_).head
     solution = Solution()
-    answer = solution.removeNthFromEnd_recursive_shift(head, n)
+    answer = solution.removeNthFromEnd_value_shifting(head, n)
     print(answer)
