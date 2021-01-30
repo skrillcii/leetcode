08f5fd948789d5b8033fb05a0a8b3335176f936c
skrillcii <skrillcii.github.com>
2021-01-30 13:32:38 +0900

    updated

--- easy/0020_ValidParentheses.py
+++ easy/0020_ValidParentheses.py
@@ -1,6 +1,10 @@
 class Solution:
 
-    def validParentheses(self, s: str) -> bool:
+    def validParentheses_inside_out(self, s: str) -> bool:
+        '''
+        Time Complexity = O(N)
+        Space Complexity = O(1)
+        '''
 
         if len(s) % 2 != 0:
             return False
@@ -12,14 +16,45 @@ class Solution:
             else:
                 return False
 
+    def validParentheses_stack(self, s: str) -> bool:
+        '''
+        Time Complexity = O(N)
+        Space Complexity = O(N)
+        '''
+        stack = []
+        mapping = {')':'(', ']':'[', '}':'{'}
+        for char in s:
+            if char in mapping.values():
+                stack.append(s)
+            elif char in mapping.keys():
+                if stack == [] or dict[char] != stack.pop()
+                    return False
+            else:
+                return False
+        return stack == []
+
 
 if __name__ == '__main__':
 
     import ipdb
     ipdb.set_trace()
 
-    s = '([])'
-
+    s = '()'
     solution = Solution()
-    answer = solution.validParentheses(s)
+    answer = solution.validParentheses_stack(s)
+    print(answer)
+
+    s = '()[]{}'
+    solution = Solution()
+    answer = solution.validParentheses_stack(s)
+    print(answer)
+
+    s = '([)]'
+    solution = Solution()
+    answer = solution.validParentheses_stack(s)
+    print(answer)
+
+    s = '([])'
+    solution = Solution()
+    answer = solution.validParentheses_stack(s)
     print(answer)
